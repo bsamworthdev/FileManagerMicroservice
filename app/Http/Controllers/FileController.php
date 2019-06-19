28870711662a3c2b73d0e707064775bd3e65fc03
vagrant <vagrant@homestead>
2019-06-19 16:11:50 +0000

    Added and implemented api endpoints

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\RecursiveIteratorIterator;
+use App\Http\Controllers\RecursiveDirectoryIterator;
+use App\Http\Controllers\FileSystemIterator;
 
 class FileController extends Controller
 {
@@ -13,18 +16,28 @@ class FileController extends Controller
     }
 
     public function uploadFile(Request $request) {
-        // $fileName = date('Ymd_His').'_upload';
-        $fileName = $request->file('photo')->getClientOriginalName();
-        $path = $request->file('photo')->move(public_path('/uploads/'), $fileName);
-        $photoURL = url('api/file/'.$fileName);//url('/uploads/'.$fileName);
-        return response()->json(['url' => $photoURL], 200);
+        $fileName = $request->file('uploaded_file')->getClientOriginalName();
+        
+        //Move file to public/uploads folder
+        $path = $request->file('uploaded_file')->move(public_path('/uploads/'), $fileName);
+
+        //Fetch url and path
+        $url = url('api/file/'.$fileName);
+        $path = '/uploads/'.$fileName;
+
+        return response()->json([
+            'url' => $url, 
+            'path' => $path
+        ], 200);
     }
 
     public function deleteFile($fileName) {
         $file_path = public_path('/uploads/').$fileName;
         if(File::exists($file_path)){
+            //Delete File
             File::delete($file_path);
         } else {
+            //File does not exist
             return response()->json([
                 'status' => 'error', 
                 'msg' => 'file not found'
@@ -38,5 +51,20 @@ class FileController extends Controller
              $files[] = $file->getFilename();
         }
         return response()->json(['files' => $files], 200);
+    }
+
+    public function getStorageSpace() {
+        $size = $this->CalculateFolderSize(public_path('/uploads/'));
+        return response()->json(['bytes' => $size], 200);
+    }
+
+    public function CalculateFolderSize($path) {
+        $bytesTotal = 0;
+        $path = realpath($path);
+        if($path!==false && $path!='' && File::exists($path)){
+            foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS)) as $obj){
+                $bytesTotal += $obj->getSize();
+            }
+        }$bytesTotal;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,24 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Retrieve Total Used Storage Space
+Route::get('file/space', 'FileController@getStorageSpace');
+
+//Get List Of Stored Files
 Route::get('file/all', 'FileController@getAllFiles');
+
+//Download File
 Route::get('file/{fileName}', 'FileController@downloadFile');
+
+//Store File
 Route::post('file', 'FileController@uploadFile');
+
+//Delete File
 Route::delete('file/{fileName}', 'FileController@deleteFile');
+
+
+
+
+
+
+
